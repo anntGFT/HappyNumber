70d852b72ebe4dc355cf962144ce6f55d4bd999d
annt <annt@gft.com>
2022-12-05 12:01:18 +0000

    Done With Recursion

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,28 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public boolean isHappy(int n) {
+
+        int sum = 0;
+
+        if (n == 1 || n == 7)
+            return true;
+
+        if (n < 10)
+            return false;
+
+        while (n > 0) {
+
+            int aux = n % 10;
+
+            sum += Math.pow(aux, 2);
+            n /= 10;
+        }
+
+        return isHappy(sum);
     }
 }
--- src/test/java/com/example/SolutionTest.java
+++ src/test/java/com/example/SolutionTest.java
@@ -1,2 +1,61 @@
-package com.example;public class SolutionTest {
+package com.example;
+
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int n;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void ZeroIsNotAHappyNumber(){
+
+        n = 0;
+
+        assertFalse(solution.isHappy(n));
+    }
+
+    @Test
+    public void OneIsAHappyNumber(){
+
+        n = 1;
+
+        assertTrue(solution.isHappy(n));
+    }
+
+    @Test
+    public void TwoIsNotAHappyNumber(){
+
+        n = 2;
+
+        assertFalse(solution.isHappy(n));
+    }
+
+    @Test
+    public void SevenIsAHappyNumber(){
+
+        n = 7;
+
+        assertTrue(solution.isHappy(n));
+    }
+
+    @Test
+    public void NineteenIsAHappyNumber(){
+
+        n = 19;
+
+        assertTrue(solution.isHappy(n));
+    }
 }
